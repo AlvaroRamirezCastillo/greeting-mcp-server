f79cd18fd2321b42c01ade9a38c4b22ca90a1b58
Alvaro Ramirez Castillo <alvaro@MacBook-Pro-de-Alvaro.local>
2025-10-06 16:58:29 -0500

    11

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+#!/usr/bin/env node
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
 
-// Create server instance
 const server = new McpServer({
   name: "greeting",
   version: "1.0.0",
